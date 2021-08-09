ee0addbcf32bd597d25f28452270db09ca4e20d2
Bronstering, Lukas <lukas.bronstering@vectron.de>
2021-08-09 14:19:24 +0200

    First GUI <- -> fixed

--- gui.py
+++ gui.py
@@ -4,34 +4,32 @@ import PySimpleGUI
 def mov_gui():
     file_line_of_column = [
         [
-            PySimpleGUI.Button(),
+            PySimpleGUI.Button("   "),
             PySimpleGUI.Button("▲"),
-            PySimpleGUI.Button()
+            PySimpleGUI.Button("   ")
         ]
     ]
 
     second_line_of_column = [
         [
             PySimpleGUI.Button("◄"),
-            PySimpleGUI.Button(),
+            PySimpleGUI.Button("   "),
             PySimpleGUI.Button("►")
         ]
     ]
 
     third_line_of_column = [
         [
-            PySimpleGUI.Button(""),
-            PySimpleGUI.Button(""),
+            PySimpleGUI.Button("   "),
             PySimpleGUI.Button("▼"),
+            PySimpleGUI.Button("   "),
         ]
     ]
 
     layout = [
-        [
-            PySimpleGUI.Column(file_line_of_column),
-            PySimpleGUI.Column(second_line_of_column),
-            PySimpleGUI.Column(third_line_of_column),
-        ]
+        [PySimpleGUI.Column(file_line_of_column)],
+        [PySimpleGUI.Column(second_line_of_column)],
+        [PySimpleGUI.Column(third_line_of_column)]
     ]
 
     window = PySimpleGUI.Window("Image Viewer", layout)
